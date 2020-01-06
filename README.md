9a723fe19069b76055010dfac6a617eb4ea29f40
龙柏龙 <lonbolong@outlook.com>
2020-01-06 16:14:16 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,31 @@
+# mt-nuxt-app
+
+> My neat Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+
+
+## 解决不支持 import 引入问题
+- 使用 `babel-node` 在 `package.json` 里 `dev&start` 都添加 `--exec babel-node` 
+- 创建 `.babelrc` 文件 配置 `{ "presets": ["es2015"] }`
+- 安装 `npm i babel-preset-es2015` `npm install -g babel-cli`
+
+## 安装支持 scss
+- `npm i sass-loader node-sass` 
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+
+  /*
+   ** Global CSS https://dev.iviewui.com/articles/1024499044308881408
+   */
+  css: ["iview/dist/styles/iview.css"],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["@/plugins/iview"],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+
+  /*
+   ** Build configuration https://zh.nuxtjs.org/api/configuration-build#extend
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // 当前版本没有写
+      // if( ctx.isDev && ctx.isClient){
+      //   config.module.rules.push({
+      //     enfore:'pre',
+      //     test: /\.(js|vue)$/,
+      //     loader: 'eslint-loader',
+      //     exclude: /(node_modules)/
+      //   })
+      // }
+    },
+    cache: true // 启动缓存 https://zh.nuxtjs.org/api/configuration-build#cache
+  }
+};
